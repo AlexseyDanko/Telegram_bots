8a1870e3fd593178fb3b3cf2cb8c63c8063aa43a
Alexsey <114829983+AlexseyDanko@users.noreply.github.com>
2023-06-09 23:30:20 +0300

    Update main.py

--- jokes_bot_pars/main.py
+++ jokes_bot_pars/main.py
@@ -1,1 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import telebot
+import random
 
+URL = 'https://anekdotov.net/anekdot/'
+api_key = '#########################'# api your telegram bot
+
+
+def parsing(URL):
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'lxml')
+    base = soup.find_all('div', class_='anekdot')
+    return [i.text.replace('\n', "") for i in base]
+
+
+list_jokes = parsing(URL)
+random.shuffle(list_jokes)
+
+bot = telebot.TeleBot(api_key)
+
+
+@bot.message_handler(commands=['start'])
+def hello(message):
+    bot.send_message(message.chat.id, 'Hello, please enter any number: ')
+
+
+@bot.message_handler(content_types=['text'])
+def jokes(message):
+    if message.text.lower() in '123456789':
+        bot.send_message(message.chat.id, list_jokes[0])
+        del list_jokes[0]
+    else:
+        'Please enter any number'
+
+
+bot.polling()
